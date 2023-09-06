1354e5ca2459b7ed770acc05035950e0e951830b
Vasiliy Manuilov <v.manyilov@yandex.ru>
2023-09-06 21:23:16 +0300

    добавить секцию aboutMe techs portfolio

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,17 +1,17 @@
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
-// import Techs from '../Techs/Techs';
-// import AboutMe from '../AboutMe/AboutMe';
-// import Portfolio from '../Portfolio/Portfolio';
+import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 
 function Main() {
   return (
     <main>
     <Promo />    
     <AboutProject />
-    {/* <Techs />
+    <Techs />
     <AboutMe />
-    <Portfolio /> */}
+    <Portfolio />
   </main>
   );
 }
