700b11da3feef171b17c8238257cca99b315d660
Vasiliy Manuilov <v.manyilov@yandex.ru>
2023-10-03 14:56:39 +0300

    дизаблд кнопки формы

--- src/components/FormEntrance/FormEntrance.js
+++ src/components/FormEntrance/FormEntrance.js
@@ -1,39 +1,50 @@
-import { Link } from "react-router-dom";
-import { LogoLink } from "../LogoLink/LogoLink";
-import "./FormEntrance.css"
+import { Link } from 'react-router-dom';
+import { LogoLink } from '../LogoLink/LogoLink';
+import './FormEntrance.css';
 
 export function FormEntrance({
-    children,
-    title,
-    submitText,
-    footerText,
-    footerLinkUrl,
-    footerLinkText,
-    isValid,
-    errorMessage,
-    onSubmit,
+  children,
+  title,
+  submitText,
+  footerText,
+  footerLinkUrl,
+  footerLinkText,
+  isValid,
+  errorMessage,
+  onSubmit,
 }) {
-
-    const submit = (event) => {
-        event.preventDefault();
-        if (isValid) {
-            onSubmit()
-        }
+  const submit = (event) => {
+    event.preventDefault();
+    if (isValid) {
+      onSubmit();
     }
+  };
 
-    return <section className='form-entrance'>
-        <LogoLink />
-        <h2 className='form-entrance__title'>{title}</h2>
-        <form noValidate onSubmit={submit}>
-            { children }
-            { errorMessage && <div className='form-entrance__response-error'>{ errorMessage }</div> }
-            <button disabled={!isValid} type='submit' className='form-entrance__btn-submit'>
-                {submitText}
-            </button>
-        </form>
-        <p className='form-entrance__text-under-btn'>
-            { footerText }
-            <Link to={footerLinkUrl} className='form-entrance__link'>{footerLinkText}</Link>
-        </p>
+  return (
+    <section className='form-entrance'>
+      <LogoLink />
+      <h2 className='form-entrance__title'>{title}</h2>
+      <form noValidate onSubmit={submit}>
+        {children}
+        {errorMessage && (
+          <div className='form-entrance__response-text'>{errorMessage}</div>
+        )}
+        <button
+          disabled={!isValid}
+          type='submit'
+          className={`form-entrance__btn-submit ${
+            !isValid && 'form-entrance__btn-submit_disabled'
+          }`}
+        >
+          {submitText}
+        </button>
+      </form>
+      <p className='form-entrance__text-under-btn'>
+        {footerText}
+        <Link to={footerLinkUrl} className='form-entrance__link'>
+          {footerLinkText}
+        </Link>
+      </p>
     </section>
+  );
 }
