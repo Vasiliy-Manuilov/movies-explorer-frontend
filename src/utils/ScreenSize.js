311ac44180bc92934035710be78296969d776629
Vasiliy Manuilov <v.manyilov@yandex.ru>
2023-10-11 21:21:24 +0300

    отображение количества рядов карчточек как в макете

--- src/utils/ScreenSize.js
+++ src/utils/ScreenSize.js
@@ -3,11 +3,11 @@ export const MEDIUM_SCREEN_SIZE = "medium";
 export const SMALL_SCREEN_SIZE = "small";
 
 export function getScreenSizeByWidth(width) {
-    if (width > 865) {
+    if (width >= 1280) {
         return LARGE_SCREEN_SIZE;
-    } else if (width < 865 && width > 600) {
+    } else if (width >= 748) {
         return MEDIUM_SCREEN_SIZE;
-    } else if (width < 600) {
+    } else {
         return SMALL_SCREEN_SIZE;
     }
 }
