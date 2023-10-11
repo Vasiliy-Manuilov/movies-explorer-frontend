6ee37cacb422517d9def6446b04373cfc8f2fee8
Vasiliy Manuilov <v.manyilov@yandex.ru>
2023-10-11 22:18:33 +0300

    вынос числовых данных в отдельный файл констант для лимита карточек и пагинации

--- src/utils/MoviesPaginationLimitsByScreen.js
+++ src/utils/MoviesPaginationLimitsByScreen.js
@@ -1,7 +1,28 @@
-import { LARGE_SCREEN_SIZE, MEDIUM_SCREEN_SIZE, SMALL_SCREEN_SIZE } from "./ScreenSize";
+import {
+  LARGE_SCREEN_SIZE,
+  MEDIUM_SCREEN_SIZE,
+  SMALL_SCREEN_SIZE,
+} from './ScreenSize';
+import {
+  NUMBER_CARDS_SCREEN_1280,
+  NUMBER_CARDS_SCREEN_768,
+  NUMBER_CARDS_SCREEN_320,
+  PAGINATION_CARDS_1280,
+  PAGINATION_CARDS_768,
+  PAGINATION_CARDS_320,
+} from './constants';
 
 export const limitsByScreenSize = {
-    [LARGE_SCREEN_SIZE]: { limit: 16, paginationLimit: 4 },
-    [MEDIUM_SCREEN_SIZE]: { limit: 8, paginationLimit: 2 },
-    [SMALL_SCREEN_SIZE]: { limit: 5, paginationLimit: 1 },
+  [LARGE_SCREEN_SIZE]: {
+    limit: NUMBER_CARDS_SCREEN_1280,
+    paginationLimit: PAGINATION_CARDS_1280,
+  },
+  [MEDIUM_SCREEN_SIZE]: {
+    limit: NUMBER_CARDS_SCREEN_768,
+    paginationLimit: PAGINATION_CARDS_768,
+  },
+  [SMALL_SCREEN_SIZE]: {
+    limit: NUMBER_CARDS_SCREEN_320,
+    paginationLimit: PAGINATION_CARDS_320,
+  },
 };
--- src/utils/ScreenSize.js
+++ src/utils/ScreenSize.js
@@ -1,13 +1,15 @@
-export const LARGE_SCREEN_SIZE = "large";
-export const MEDIUM_SCREEN_SIZE = "medium";
-export const SMALL_SCREEN_SIZE = "small";
+import { SCREEN_SIZE_1280, SCREEN_SIZE_748 } from './constants';
+
+export const LARGE_SCREEN_SIZE = 'large';
+export const MEDIUM_SCREEN_SIZE = 'medium';
+export const SMALL_SCREEN_SIZE = 'small';
 
 export function getScreenSizeByWidth(width) {
-    if (width >= 1280) {
-        return LARGE_SCREEN_SIZE;
-    } else if (width >= 748) {
-        return MEDIUM_SCREEN_SIZE;
-    } else {
-        return SMALL_SCREEN_SIZE;
-    }
+  if (width >= SCREEN_SIZE_1280) {
+    return LARGE_SCREEN_SIZE;
+  } else if (width >= SCREEN_SIZE_748) {
+    return MEDIUM_SCREEN_SIZE;
+  } else {
+    return SMALL_SCREEN_SIZE;
+  }
 }
